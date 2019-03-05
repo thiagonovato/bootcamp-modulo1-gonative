327fd41cf3908a8cf67a23207dd0bc31cfe334f6
Thiago Novato <thiagonovato@gmail.com>
2019-03-05 08:40:28 -0300

    Projeto inicial. Passando props direto via component. DefaultProps.

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+
+import Todo from "./components/Todo";
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Todo title="Fazer café" />
+        <Todo />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  }
+});
