6356ca5881536d7fd1261bc4596547389705ce9f
Thiago Novato <thiagonovato@gmail.com>
2019-03-05 08:45:49 -0300

    Definindo proptypes.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+
 import { View, Text, StyleSheet } from "react-native";
 
 export default class Todo extends Component {
   static defaultProps = {
     title: "Todo padrão"
   };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired
+  };
+
   render() {
     return (
       <View>
